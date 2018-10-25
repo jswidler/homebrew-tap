afe4eb2851da032fa0701e6b3ec6f5cf06bba1cd
Jesse Swidler <jesse@udacity.com>
2018-10-24 18:35:51 -0700

    lockgit; update to 0.5.1

--- lockgit.rb
+++ lockgit.rb
@@ -1,19 +1,19 @@
 require "rbconfig"
 class Lockgit < Formula
-  desc "A CLI tool for storing encrypted secrets in a git repo"
+  desc "a CLI tool for storing encrypted secrets in a git repo"
   homepage "https://github.com/jswidler/lockgit"
-  version "0.5.0"
+  version "0.5.1"
 
   if Hardware::CPU.is_64_bit?
     case RbConfig::CONFIG["host_os"]
     when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
       :windows
     when /darwin|mac os/
-      url "https://github.com/jswidler/lockgit/releases/download/v0.5.0/lockgit_0.5.0_darwin_amd64.zip"
-      sha256 "c9bffa00f6208b66e8c3549413951c0b57b89302c4ec66c0265a65c74664dc72"
+      url "https://github.com/jswidler/lockgit/releases/download/v0.5.1/lockgit_0.5.1_darwin_amd64.zip"
+      sha256 "bb54a5db8e22428496ea07dd504050022b86714d9441a81c203edf9d663316f3"
     when /linux/
-      url "https://github.com/jswidler/lockgit/releases/download/v0.5.0/lockgit_0.5.0_linux_amd64.tar.gz"
-      sha256 "f5333f6a3bd70d42bf3a5ec48835b45c0ea3579b1b6ea775688d58f39cab83de"
+      url "https://github.com/jswidler/lockgit/releases/download/v0.5.1/lockgit_0.5.1_linux_amd64.tar.gz"
+      sha256 "41e5620c8266fa359aed29bd64739028315977d7165b5cf7963001342b34c01c"
     when /solaris|bsd/
       :unix
     else
@@ -24,11 +24,11 @@ class Lockgit < Formula
     when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
       :windows
     when /darwin|mac os/
-      url "https://github.com/jswidler/lockgit/releases/download/v0.5.0/lockgit_0.5.0_darwin_386.zip"
-      sha256 "a1446051985fd45a3f60d163529526b2fa4dcfda9428e7b75d4480641d15330d"
+      url "https://github.com/jswidler/lockgit/releases/download/v0.5.1/lockgit_0.5.1_darwin_386.zip"
+      sha256 "07a898d7bb41b6740d62187e6c74105b0e634ad65c70b897959ec01f549942d1"
     when /linux/
-      url "https://github.com/jswidler/lockgit/releases/download/v0.5.0/lockgit_0.5.0_linux_386.tar.gz"
-      sha256 "2dc2eb2e05f9c133e776194b314fe79893f2ca68b29fa47ad1accc024d231830"
+      url "https://github.com/jswidler/lockgit/releases/download/v0.5.1/lockgit_0.5.1_linux_386.tar.gz"
+      sha256 "2c4cd974c1282cef384e82db9bd3aa13bd91c5604a458435f2ff5336a873a9c0"
     when /solaris|bsd/
       :unix
     else
